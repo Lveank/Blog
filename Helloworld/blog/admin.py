23516e5da996c5b72ce1966e855aad3c7bb6aec3
Lveank <zhaoyichen71@gmail.com>
2018-06-24 01:54:28 +0800

    简单博客系统的搭建

--- Helloworld/blog/admin.py
+++ Helloworld/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# 将BlogArticles类引入当前环境
+from .models import BlogArticles
+
+
+# 为了丰富管理员列表中的信息，定义一个新类
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publish")
+    list_filter = ("publish", "author")
+    search_fields = ("title", "body")
+    raw_id_fields = ("author",)
+    date_hierarchy = "publish"
+    ordering = ["publish", "author"]
+
+
+# 将BlogArticles类注册到admin中
+admin.site.register(BlogArticles, BlogArticlesAdmin)
--- Helloworld/blog/views.py
+++ Helloworld/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import BlogArticles
+
+
+def blog_title(request):
+    """响应用户请求，展示文章标题"""
+    blogs = BlogArticles.objects.all()
+    # render：将数据渲染到模板上
+    return render(request, "blog/titles.html", {"blogs": blogs})
+
+
+def blog_article(request, article_id):
+    # article = BlogArticles.objects.get(id=article_id)
+    article = get_object_or_404(BlogArticles, id=article_id)  # 对异常请求进行处理
+    pub = article.publish
+    return render(request, "blog/content.html", {"article": article, "publish": pub})
