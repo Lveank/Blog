9ac300224898b01f65277e38a6e1ae132aa4bc90
Lveank <zhaoyichen71@gmail.com>
2018-06-26 23:40:52 +0800

    修改模板文件
    删除<li><a href="{{ blog.title }}">{{ blog.title }}</a></li>
    url配置依然有问题

--- Helloworld/blog/urls.py
+++ Helloworld/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.blog_title, name='blog_title'),
-    url(r'^$', views.blog_title, name='blog'),
+    # url(r'^$', views.blog_title, name='blog'),
     url(r'(?P<article_id>\d)/$', views.blog_article, name='blog_detail'),
 ]
