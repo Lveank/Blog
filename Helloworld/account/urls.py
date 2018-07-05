aaa6e75966c847066b2950b6541f89dbaf681596
Lveank <zhaoyichen71@gmail.com>
2018-07-05 17:32:50 +0800

    内置的登录方法
    两种方法：①修改settings和模板文件中的POST目标；②增加一个URL配置

--- Helloworld/account/urls.py
+++ Helloworld/account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 from django.conf import settings
 
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views  # 为了使用内置的登陆方法，导入了Django内置视图文件
 
 # from blog.views import blog_title, blog_article
 
@@ -11,5 +11,6 @@ urlpatterns = [
     # url(r'^$', views.index, name='index'),
     # url(r'^login/$', views.user_login, name='user_login'),  # 自定义的登录
     url(r'^login/$', auth_views.login, name='user_login'),  # django内置登录
-
+    # 以一个字典类型，向默认的auth_views.login传template_name的值
+    url(r'new-login/$', auth_views.login, {'template_name': 'account/login.html'}),  # django内置登陆方法2
 ]
