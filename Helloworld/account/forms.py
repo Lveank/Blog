904e3a95d50b78647e46839836eafa719348ec49
Lveank <zhaoyichen71@gmail.com>
2018-07-06 20:14:27 +0800

    用户注册1
    创建一个注册用户的表单类

--- Helloworld/account/forms.py
+++ Helloworld/account/forms.py
@@ -2,7 +2,26 @@
 
 from django import forms
 
+# 引入Django默认的用户模型User类
+from django.contrib.auth.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+# 增加一个注册用户的表单类
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("password do not match")
+        return cd['password2']
