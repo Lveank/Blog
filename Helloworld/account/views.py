842b7d440f761217c6cca0d369591c7293b0d6be
Lveank <zhaoyichen71@gmail.com>
2018-07-06 21:04:52 +0800

    用户注册2
    创建注册的视图函数

--- Helloworld/account/views.py
+++ Helloworld/account/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+
 
 def user_login(request):
     if request.method == "POST":
@@ -21,4 +22,19 @@ def user_login(request):
 
     if request.method == "GET":
         login_form = LoginForm()
-        return render(request, "account/login.html", {"form":login_form})
+        return render(request, "account/login.html", {"form": login_form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponse("Registered successfully")
+        else:
+            return HttpResponse("Sorry, you can't register.")
+    else:
+        user_form = RegistrationForm()
+        return render(request, "account/register.html", {"form": user_form})
